cdeb829322b665f6d3183088fdf974e7ccf6c2ea
Megumi Yanaka <margaret_mori0102@yahoo.com>
2022-06-03 14:37:16 +0100

    Level100

--- script.js
+++ script.js
@@ -1,12 +1,49 @@
-//You can edit ALL of the code here
 function setup() {
-  const allEpisodes = getAllEpisodes();
-  makePageForEpisodes(allEpisodes);
+  const episodes = getAllEpisodes();
+  makePageForEpisodes(episodes);
 }
 
 function makePageForEpisodes(episodeList) {
-  const rootElem = document.getElementById("root");
-  rootElem.textContent = `Got ${episodeList.length} episode(s)`;
+  const container = document.getElementById("episodes");
+
+  //Nav -- Title and Search bar
+  const navConrainer = document.createElement("div");
+  navConrainer.classList.add("nav-container");
+
+  const title = document.createElement("div");
+  title.classList.add("title");
+  title.textContent = 'TV SHOW';
+
+  container.appendChild(navConrainer);
+  navConrainer.appendChild(title);
+
+//-------------------------------------
+// Cards
+  const cardsContainer = document.createElement("div");
+  cardsContainer.classList.add("cards-container");
+  container.appendChild(cardsContainer);
+// One card 
+  episodeList.forEach((episode) => {
+    const card = document.createElement("div");
+    card.classList.add("card");
+
+    const h1 = document.createElement("h1");
+    h1.textContent = `${episode.name}`;
+
+    const img = document.createElement("img");
+    img.setAttribute("src", episode.image.medium);
+
+    const p = document.createElement("p");
+    p.textContent = episode.summary;
+
+    cardsContainer.appendChild(card);
+    card.appendChild(h1);
+    card.appendChild(img);
+    card.appendChild(p);
+  //-------------------------------------
+  });
 }
+
+
 
 window.onload = setup;
